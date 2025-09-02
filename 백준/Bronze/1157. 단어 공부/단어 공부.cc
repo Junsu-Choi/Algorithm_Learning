865bdb415bd175acc3752f5879615f4b6ebc9353
Junsu-Choi <96176789+Junsu-Choi@users.noreply.github.com>
2025-09-02 12:42:11 +0900

    [Bronze I] Title: 단어 공부, Time: 52 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+#define x first
+#define y second
+#define all(v) v.begin(), v.end()
+#define compress(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+#define lower(v, x) (int)(lower_bound(all(v), x) - v.begin())
+#define upper(v, x) (int)(upper_bound(all(v), x) - v.begin())
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    string s;
+    cin >> s;
+
+    transform(all(s), s.begin(), ::toupper);
+
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    vector<int> v;
+    v.resize(30);
+    for (int i=0; i<s.length(); i++) {
+        for (int j=0; j<alphabet.length(); j++) {
+            if (s[i] == alphabet[j]) {
+                v[j]++;
+            }
+        }
+    }
+
+    int max=0, duplicateData=-1;
+    int maxIndex;
+    for (int i=0; i<alphabet.length(); i++) {
+        if (max < v[i]) {
+            max = v[i];
+            maxIndex = i;
+        }
+        else if (max == v[i]) {
+            duplicateData = v[i];
+        }
+    }
+
+    if (max == duplicateData) {
+        cout << "?" << endl;
+    }
+    else cout << alphabet[maxIndex] << endl;
+
+    exit(0);
+}
